516623bee6c2e16f3d4e2485657e44a86a10b488
quandtifier <quandtifier@gmail.com>
2018-04-17 10:46:11 -0700

    added the CreateAccountActivity

--- app/src/main/java/edu/tacoma/uw/css/quandm/chainganggamma/LoginActivity.java
+++ app/src/main/java/edu/tacoma/uw/css/quandm/chainganggamma/LoginActivity.java
@@ -1,7 +1,9 @@
 package edu.tacoma.uw.css.quandm.chainganggamma;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void createAccountNav(View view) {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
     }
 }
